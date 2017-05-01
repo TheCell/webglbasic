e6f87fa2d91c6ea9d628484ff381246924191014
Simon <simon_hischier@hotmail.com>
2017-05-01 10:23:41 +0200

    Color for every Vertex

--- Projekt2/Exercise1.js
+++ Projekt2/Exercise1.js
@@ -14,7 +14,9 @@ var gl;
 var ctx = {
     shaderProgram: -1,
     aVertexPositionId: -1,
-    uColorId: -1
+    uColorId: -1,
+    aVertexColorId: -1,
+    verticesAndColors: -1
 };
 
 /**
@@ -46,7 +48,8 @@ function initGL() {
 function setUpAttributesAndUniforms(){
     "use strict";
     ctx.aVertexPositionId = gl.getAttribLocation(ctx.shaderProgram, "aVertexPosition");
-    ctx.uColorId = gl.getUniformLocation(ctx.shaderProgram, "uColor");
+    //ctx.uColorId = gl.getUniformLocation(ctx.shaderProgram, "uColor");
+    ctx.aVertexColorId = gl.getAttribLocation(ctx.shaderProgram, "aVertexColor");
 }
 
 /**
@@ -56,18 +59,28 @@ function setUpBuffers(){
     "use strict";
     rectangleObject.buffer = gl.createBuffer();
 
+    /*
     var vertices = [
     -0.2, 0.5,
     0.2, 0.5,
     0.2, -0.5,
     -0.2, -0.5
     ]
-    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-    gl.uniform4f(ctx.uColorId, 1, 0, 1, 1)
-    /*
     */
+
+    // x, y, r, g, b, a
+    ctx.verticesAndColors = [
+        -0.2, 0.5, 1, 0.2, 1, 1,
+        0.2, 0.5, 0.3, 0.2, 0, 1,
+        0.2, -0.5, 0, 1, 1, 1,
+        -0.2, -0.5, 1, 1, 1, 1
+    ];
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
+    //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(ctx.verticesAndColors), gl.STATIC_DRAW);
+
+    //gl.uniform4f(ctx.uColorId, 1, 0, 1, 1);
 }
 
 /**
@@ -75,14 +88,19 @@ function setUpBuffers(){
  */
 function draw() {
     "use strict";
+    // set background color
     gl.clearColor(0.3, 1, 0.3, 1);
+    // clear image with defined color
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    /*
-    */
+    // bind buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
-    gl.vertexAttribPointer(ctx.aVertexPositionId, 2, gl.FLOAT, false, 0, 0);
+    // Pointer data
+    gl.vertexAttribPointer(ctx.aVertexPositionId, 2, gl.FLOAT, false, 24, 0);
+    gl.vertexAttribPointer(ctx.aVertexColorId, 2, gl.FLOAT, false, 24, 8);
+    //
     gl.enableVertexAttribArray(ctx.aVertexPositionId);
+    gl.enableVertexAttribArray(ctx.aVertexColorId);
 
     gl.drawArrays(gl.LINE_LOOP, 0, 4);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
